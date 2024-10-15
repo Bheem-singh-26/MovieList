095cc384b5b82b20a54ec189cb1f9c9eac942e7e
Bheem-singh-26 <bheem.r.singh@gmail.com>
2024-10-15 17:10:00 +0530

    created project for task

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Bheem Singh on 15/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
